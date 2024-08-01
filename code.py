0735ba507d23592ec4e7cb885565143de34c26a7
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-01 13:59:38 +0530

    Update code.py input for grps

--- code.py
+++ code.py
@@ -6,3 +6,18 @@ onlyGroupC=[]
 a=int(input("How many students are there in group A:"))
 b=int(input("How many students are there in group B:"))
 c=int(input("How many students are there in group C:"))
+
+for i in range(a):
+	name=input("Enter name of the student:")
+	groupA.append(name)
+print("Group A =",groupA)
+
+for i in range(b):
+	name=input("Enter name of the student:")
+	groupB.append(name)
+print("Group B =",groupB)
+
+for i in range(c):
+	name=input("Enter name of the student:")
+	groupC.append(name)
+print("Group C =",groupC)

943563e0870a63a015867739853047c8313da239
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-01 13:58:27 +0530

    Update code.py empty lists

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
+groupA=[]
+groupB=[]
+groupC=[]
+onlyGroupC=[]
 
+a=int(input("How many students are there in group A:"))
+b=int(input("How many students are there in group B:"))
+c=int(input("How many students are there in group C:"))
